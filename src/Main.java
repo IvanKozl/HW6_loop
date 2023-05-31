1f5c6d1ec9b027f242efe0c7f76a173895dbc7bc
velik <velikiinkvizitor@mail.ru>
2023-06-01 01:04:07 +0300

    Check HW6_loop улучшил проценты после видео

--- src/Main.java
+++ src/Main.java
@@ -42,9 +42,11 @@ public class Main {
 
     public static void task5() {
         System.out.println("Задача 5");
-        for (int i = 1904; i <= 2096; i = i + 4) {
-            System.out.println(i + " год является високосным");
+        for (int year = 1904; year <= 2096; year++) {
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            System.out.println(year + " год является високосным");
         }
+
     }
         public static void task6() {
             System.out.println("Задача 6");
@@ -64,26 +66,26 @@ public class Main {
             int salary = 29_000;
             int total = 0;
             int month = 12;
-            int firstMonthMoney = 0;
+            int firstMonthMoney = 1;
             for (int i = firstMonthMoney; i <= month; i++) {
-                total = total + salary;
-            System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+                total += salary;
+                System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
             }
         }
         public static void task9() {
             System.out.println("Задача 9");
             int salary = 29_000;
-            float total = 0;
-            float percent = 100;
-            for (int i = 0; i <= 12; i++) {
-                total = (total + total/percent);
-                total = total + salary;
+            double total = 0;
+            int month = 12;
+            int firstMonthMoney = 1;
+            double percent = 0.12/12 + 1;
+            for (int i = firstMonthMoney; i <= month; i++) {
+                total += salary * percent;
                 System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
             }
+            }
 
-        }
-
-        public static void task10() {
+            public static void task10() {
             System.out.println("Задача 10d");
             int i = 1;
             int multiplication = 2;
